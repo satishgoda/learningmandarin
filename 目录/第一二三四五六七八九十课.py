1b7f701907aee62108436ee1e9260fdae594bbe9
Satish Goda <satishgoda@live.com>
2015-08-24 22:59:13 +0800

    pinyin and characters splitting 第一二三四五六七八九十课

---
+++ 目录/第一二三四五六七八九十课.py
@@ -0,0 +1,12 @@
+tenChapters = """di4yi1ke4   第一课
+di4er4ke4    第二课
+di4san1ke4  第三课
+di4si4ke4   第四课
+di4wu3ke4   第五课
+di4liu4ke4  第六课
+di4qi1ke4   第七课
+di4ba1ke4   第八课
+di4jiu3ke4  第九课
+di4shi2ke4  第十课"""
+
+print map(str.split, tenChapters.split("\n"))
